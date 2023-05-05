931918bdd02b0c0256f636d2fbd30d61de55527e
Art <vito.reason@gmail.com>
2023-05-06 00:23:39 +0300

    add breakpoints 768

--- js/slider.js
+++ js/slider.js
@@ -1,28 +1,18 @@
 const swiper = new Swiper('.swiper', {
 	slidesPerView: 'auto',
 	loop: true,
-	autoplay: {
-		delay: 5000,
-		disableOnInteraction: false
-	 },
 	 speed: 1000,
 	 pagination: {
 		el: '.swiper-pagination',
 		clickable: true,
-		coverflowEffect: {
-		  rotate: 0,
-		  stretch: 200,
-		  depth: 200,
-		  modifier: 1,
-		  slideShadows : false
-		}
 	 },
 	 breakpoints: {
 		768 :{
-		enabled: false
+		enabled: false,
 		}
 	  }
 });
+
 
 const body = document.body
 const listShowAll = body.querySelector('.services__list')
