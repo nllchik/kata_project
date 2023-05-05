d0e6e93b7e836f47fff33d628538130a4841f5e9
Art <vito.reason@gmail.com>
2023-05-05 15:29:27 +0300

    add breakpoints 768

--- js/slider.js
+++ js/slider.js
@@ -1,31 +1,32 @@
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i
-   .test(navigator.userAgent)) {
+const swiper = new Swiper('.swiper', {
+	slidesPerView: 'auto',
+	loop: true,
+	autoplay: {
+		delay: 5000,
+		disableOnInteraction: false
+	 },
+	 speed: 1000,
+	 pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+		coverflowEffect: {
+		  rotate: 0,
+		  stretch: 200,
+		  depth: 200,
+		  modifier: 1,
+		  slideShadows : false
+		}
+	 },
+	 breakpoints: {
+		768 :{
+		enabled: false
+		}
+	  }
+});
 
-		if (window.matchMedia('(max-width: 768px)').matches) {
-			const swiper = new Swiper('.swiper', {
-			  slidesPerView: 'auto',
-			  autoplay: {
-				 delay: 5000,
-				 disableOnInteraction: false
-			  },
-			  speed: 1000,
-			  pagination: {
-				 el: '.swiper-pagination',
-				 clickable: true,
-				 coverflowEffect: {
-					rotate: 0,
-					stretch: 200,
-					depth: 200,
-					modifier: 1,
-					slideShadows : false
-				 }
-			  }
-			});
-		 }
-	}
-
-const listShowAll = document.querySelector('.services__list')
-const btnShowMore = document.querySelector('.services__btn-show-more')
+const body = document.body
+const listShowAll = body.querySelector('.services__list')
+const btnShowMore = body.querySelector('.services__btn-show-more')
 
 btnShowMore.addEventListener('click', function() {
 	btnShowMore.classList.toggle('services__btn-show-more--open')
